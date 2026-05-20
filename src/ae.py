87e42cd0c1f3eede06130d2bf6e5234464404f13
harelc <harel@lightricks.com>
2026-05-20 07:57:40 +0300

    Use sd2-community/stable-diffusion-2-1 mirror (original is deprecated/gated)

--- src/ae.py
+++ src/ae.py
@@ -3,7 +3,7 @@ import torch
 from torch import nn
 
 class AutoencoderKL(nn.Module):
-    def __init__(self, scale: float = 0.18215, shift: float = 0.0, repo="stabilityai/stable-diffusion-2-1"):
+    def __init__(self, scale: float = 0.18215, shift: float = 0.0, repo="sd2-community/stable-diffusion-2-1"):
         super().__init__()
         self.scale = scale
         self.shift = shift
